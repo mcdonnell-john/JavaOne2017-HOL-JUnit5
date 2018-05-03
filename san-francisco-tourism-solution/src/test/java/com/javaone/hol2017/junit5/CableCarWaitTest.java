ec9fd13ad5146019e85941cd744c842f1477147a
John McDonnell <john.mcdonnell@bearingpoint.com>
2018-05-03 14:12:58 +0100

    Updated to have more recent JUnit 5 versions

--- san-francisco-tourism-solution/src/test/java/com/javaone/hol2017/junit5/CableCarWaitTest.java
+++ san-francisco-tourism-solution/src/test/java/com/javaone/hol2017/junit5/CableCarWaitTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.*;
 import org.mockito.*;
 
-import name.falgout.jeffrey.testing.junit5.*;
+import name.falgout.jeffrey.testing.junit.mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CableCarWaitTest {
